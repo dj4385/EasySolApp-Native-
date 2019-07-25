0ef877d29d1467b07690a1fd09f2029fe46150d6
dj4385 <dj4385@gmail.com>
2019-07-25 11:48:44 +0530

    change the project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,10 @@ const routes: Routes = [
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
   { path: 'sales-man-login', loadChildren: './sales-man-login/sales-man-login.module#SalesManLoginPageModule' },
   { path: 'home-page', loadChildren: './home-page/home-page.module#HomePagePageModule' },
-  { path: 'about-us', loadChildren: './home-page/about-us/about-us.module#AboutUsPageModule' },
-  { path: 'contact-us', loadChildren: './home-page/contact-us/contact-us.module#ContactUsPageModule' },  { path: 'item-page', loadChildren: './home-page/item-page/item-page.module#ItemPagePageModule' },
-  { path: 'customer', loadChildren: './home-page/customer/customer.module#CustomerPageModule' },
+  { path: 'about-us', loadChildren: './about-us/about-us.module#AboutUsPageModule' },
+  { path: 'contact-us', loadChildren: './contact-us/contact-us.module#ContactUsPageModule' },
+  { path: 'item-page', loadChildren: './item-page/item-page.module#ItemPagePageModule' },
+  { path: 'customer', loadChildren: './customer/customer.module#CustomerPageModule' },
 
 
 ];
